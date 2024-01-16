da031baea384fde5939a494c8e578dffe8c833cb
lingyundai <lingyundaiseven@gmail.com>
2024-01-16 15:52:20 -0500

    setting up some things

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import HomepageContainer from "../features/Homepage/HomepageContainer"
+
+function App() {
+  return (
+    <div className="App">
+      <HomepageContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+export default function Homepage() {
+    return (
+        <div>
+            <div class="flex">
+                <div class="bg-zinc-800 w-screen h-screen bg-cover">
+
+                </div>
+            </div>
+        </div>
+    )
+}
