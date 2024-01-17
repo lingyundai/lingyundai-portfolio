6e188c3c8b9b6a0bfb2ee02dc4ce0d392be707b0
lingyundai <lingyundaiseven@gmail.com>
2024-01-17 17:02:38 -0500

    start content

--- src/features/Homepage/Homepage.js
+++ src/features/Homepage/Homepage.js
@@ -1,9 +1,13 @@
+import content from "../../common/content.json";
+
 export default function Homepage() {
     return (
         <div>
             <div class="flex">
                 <div class="bg-zinc-800 w-screen h-screen bg-cover">
-
+                    <header class="text-white">
+                        {content.Introduction}
+                    </header>
                 </div>
             </div>
         </div>
